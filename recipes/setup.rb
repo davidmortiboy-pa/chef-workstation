b9ee7b69315c96a72d1ed8712db165bb25d04ca8
David Mortiboy <david.mortiboy@pressassociation.com>
2018-11-22 16:34:07 +0000

    Updated motd to use template file, added patch version number

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,17 +1,6 @@
 package 'tree' do
   action :install
 end
-file '/etc/motd' do
-  content "Property of ...
-
-
-  IPADDRESS: #{node['ipaddress']}
-  HOSTNAME : #{node['hostname']}
-  MEMORY   : #{node['memory']['total']}
-  CPU      : #{node['cpu']['0']['mhz']}
-"
-  mode '0644'
-  owner 'root'
-  group 'root'
-  action :create
+template '/etc/motd' do
+  source 'motd.erb'
 end
